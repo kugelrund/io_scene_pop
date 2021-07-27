87ee127a70f6e7a21dff30ec3efdc8204324507a
kugelrund <6236137+kugelrund@users.noreply.github.com>
2021-07-27 22:59:44 +0200

    Bump version to 0.2

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "PoP map",
     "author": "Sphere",
-    "version": (0, 1),
+    "version": (0, 2),
     "blender": (2, 80, 0),
     "location": "File > Import > PoP map",
     "description": "Import PoP map",
